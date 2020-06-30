09d5923d9711e20a51a9b66dd82e6c820e7adf8e
ajim <n1a1o2k0i@gmail.com>
2020-06-30 10:02:18 +0900

    change main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faSignOutAlt, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+Vue.use(firestorePlugin)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXUo5msrS94WawE9kS2D7BUG-NZQIBpBk",
+  authDomain: "rumbling-roar.firebaseapp.com",
+  databaseURL: "https://rumbling-roar.firebaseio.com",
+  projectId: "rumbling-roar",
+  storageBucket: "rumbling-roar.appspot.com",
+  messagingSenderId: "244133182678",
+  appId: "1:244133182678:web:aa822a303604cb8676dd77",
+  measurementId: "G-1H0D7SEDVQ"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+
+Vue.component('fa', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
